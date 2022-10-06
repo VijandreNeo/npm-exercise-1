eebe93fdf96dfd7248ca4d55dc64b4d73bc705e4
Neo Jan Erich Vijandre <neojanerich.vijandre@neojanerich.vijandre-CH4K3HF5WM>
2022-10-06 08:14:57 +0800

    Vcard Exercise

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const sensor = require('qrcode-terminal');
 
 sensor.generate("WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:SocialD3$K09F;;");
 console.log(sensor);
+
+sensor.generate(`BEGIN:VCARD
+VERSION:4.0
+EMAIL;type=HOME:vijandreneo@gmail.com
+EMAIL;type=WORK:neojanerich.vijandre@siteminder.com
+TEL;type=WORK:09774943118
+FN:Neo Jan Erich A. Vijandre
+N:Neo Vijandre
+END:VCARD`);
