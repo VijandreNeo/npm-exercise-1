9f0989e042d67c64a7cd14720d562fc27406f81e
Neo Jan Erich Vijandre <neojanerich.vijandre@neojanerich.vijandre-CH4K3HF5WM>
2022-10-11 09:38:13 +0800

    prettier implementation

--- qr.js
+++ qr.js
@@ -1,4 +1,4 @@
 const sensor = require('qrcode-terminal');
 
-sensor.generate("WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:SocialD3$K09F;;");
+sensor.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:SocialD3$K09F;;');
 console.log(sensor);
